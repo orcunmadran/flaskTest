23a7302d88ce6debb4d55ccbc111ed64e921d456
orcunmadran <orcunmadran@gmail.com>
2024-11-27 14:36:16 +0000

    test2 is deleted

--- app.py
+++ app.py
@@ -4,25 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Test!<br><a href='test'>go to Test... </a><br><a href='test2'>go to Test2... </a>"
+    return "Flask Test!<br><a href='test'>go to Test... </a>"
 
 @app.route("/test")
 def test_route():
     data = {
-        "name": "John Doe",
-        "age": 30,
-        "city": "New York"
+        "data1": "Open Resource Portal",
+        "data2": "openresource.net"
     }
     return render_template("test.html", data=data)
-
-@app.route("/test2")
-def test2_route():
-    data = {
-        "name": "Orçun",
-        "age": 50,
-        "city": "Ankara"
-    }
-    return render_template("test2.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
