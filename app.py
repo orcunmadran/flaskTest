8d2ce47835f30110b5d691c15f31dc41b32514d0
orcunmadran <orcunmadran@gmail.com>
2024-11-27 15:00:37 +0000

    loto app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
@@ -13,6 +14,23 @@ def test_route():
         "data2": "openresource.net"
     }
     return render_template("test.html", data=data)
+
+@app.route("/loto")
+def loto_route():
+    
+    i = 0
+    secilenler = [0,0,0,0,0,0]
+    goster = []
+    for rastgele in secilenler:
+        while i < len(secilenler):
+            secilen = randint(1, 49)
+            if secilen not in secilenler:
+                secilenler[i] = secilen
+                i+=1
+        goster.append(sorted(secilenler))
+        i=0
+
+    return render_template("loto.html", data=goster)
 
 if __name__ == "__main__":
     app.run(debug=True)
