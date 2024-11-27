6dd117a51c1de61b7283a85955d1b3dc957c4e07
orcunmadran <orcunmadran@gmail.com>
2024-11-27 14:01:51 +0000

    new test page is added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Test!<br><a href='test'>go to Test... </a>"
+    return "Flask Test!<br><a href='test'>go to Test... </a><br><a href='test2'>go to Test2... </a>"
 
 @app.route("/test")
 def test_route():
@@ -14,6 +14,15 @@ def test_route():
         "city": "New York"
     }
     return render_template("test.html", data=data)
+
+@app.route("/test2")
+def test_route():
+    data = {
+        "name": "Orçun",
+        "age": 50,
+        "city": "Ankara"
+    }
+    return render_template("test2.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
