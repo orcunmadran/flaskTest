0d82fa8942780c1119f9949cc7d4b78f69ba21c0
orcunmadran <orcunmadran@gmail.com>
2024-11-27 10:54:05 +0000

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask Test!"
+    return "Flask Test!<br><a href='test'>go to Test... </a>"
 
 @app.route("/test")
 def test_route():
